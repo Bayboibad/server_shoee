5f0f178f7e97417d9138e57142c375ea0e312494
long213k@gmail.com <109726982+Bayboibad@users.noreply.github.com>
2023-11-09 18:09:35 +0700

    update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -25,36 +25,40 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-exports.addProduct = upload.array('avatar', 5), async function (req, res, next) {
-  try {
-    const productName = req.body.name;
-    const productCart = req.body.cart;
-    const productBrand = req.body.brand;
-    const productPriceNew = req.body.priceNew;
-    const productPriceOld = req.body.priceOld;
-    const productNumber = req.body.number;
-    const productDescription = req.body.description;
-    const productSizes = req.body.productSizes; // Lấy giá trị từ trường productSizes trong biểu mẫu
-    const anhDaiDien = req.files ? req.files.map(file => 'uploads/' + file.filename) : null;
-
-    // Tạo sản phẩm mới sử dụng Mongoose
-    await Product.create({
-      name: productName,
-      cart: productCart,
-      brand: productBrand,
-      priceNew: productPriceNew,
-      priceOld: productPriceOld,
-      number: productNumber,
-      description: productDescription,
-      avartarFile: anhDaiDien,
-      productSizes: productSizes,
-    });
-
-    res.redirect('/product');
-  } catch (error) {
-    res.status(500).json({ message: 'Lỗi server' });
-    next(error);
-  }
+exports.addProduct = upload.array('avatar', 5), async (req, res, next) => {
+  if(req.method =='POST')
+    {
+      try {
+        const productName = req.body.name;
+        const productCart = req.body.cart;
+        const productBrand = req.body.brand;
+        const productPriceNew = req.body.priceNew;
+        const productPriceOld = req.body.priceOld;
+        const productNumber = req.body.number;
+        const productDescription = req.body.description;
+        const productSizes = req.body.productSizes; // Lấy giá trị từ trường productSizes trong biểu mẫu
+        const anhDaiDien = req.files ? req.files.map(file => 'uploads/' + file.filename) : null;
+        // Tạo sản phẩm mới sử dụng Mongoose
+        await Product.insertMany({
+          name: productName,
+          cart: productCart,
+          brand: productBrand,
+          priceNew: productPriceNew,
+          priceOld: productPriceOld,
+          number: productNumber,
+          description: productDescription,
+          avartarFile: anhDaiDien,
+          productSizes: productSizes,
+        });
+    
+        res.redirect('/product');
+      } catch (error) {
+        res.status(500).json({ message: 'Lỗi server' });
+        next(error);
+      }
+    }else{
+      res.redirect('/product');
+    }
 };
 
 
